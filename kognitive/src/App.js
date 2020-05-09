673067e6c6aa25f7b46080b713294c289638d2a0
psakhamuri <64536658+psakhamuri@users.noreply.github.com>
2020-05-09 12:44:54 -0400

    view

--- kognitive/src/App.js
+++ kognitive/src/App.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import './App.css';
+import Tasks from './Components/Tasks';
 
 function App() {
   return (
     
     <div className="App-header">
 
-      <div class="card mt-4 mr-3 ml-3">
-  <div class="card-body">
-
-  <div class="login-form">
-    <form action = "">
-    <img class="card-img-top" src="./kog_logo_flat@3x.png/" alt="Card image cap"/>
-
-    <div class="social-icon">
-        <button type="button mb-3"><span class="input-icon"><img  src="./assets/icons/othersize/google-favicon.png/"/>&nbsp;</span>Login With Google</button>
-        <button type="button"><span class="input-icon"><img  src="./assets/icons/othersize/microsoft.png/"/>&nbsp;</span>Login With Office 365</button>
-      </div>
-      <div class="seperator"><b>or</b></div>
-    <div class="form-group">
-        <input type="email" name="email" placeholder="Your Employee ID"/>
-        <span class="input-icon"><img  src="./assets/icons/24/icon-24-person-grey.png/"/></span>
-      </div>
-      <div class="form-group">
-        <input type="password" name="psw" placeholder="Your Password"/>
-        <span class="input-icon"><img src="./assets/icons/24/icon-24-password.png/"/></span>
-      </div>
-      <button class="login-btn">Login</button> 
-    </form>
-    </div>
-
-  </div>
-  </div>
+     {/* <Login/> */}
+     <Tasks/>
 </div>
   );
 }
---
+++ kognitive/src/Components/Login.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+    render(){
+  return (
+    
+    <div class="card mt-4 mr-3 ml-3">
+    <div class="card-body">
+  
+    <div class="login-form">
+      <form action = "">
+      <img class="card-img-top" src="./kog_logo_flat@3x.png/" alt=""/>
+  
+      <div class="social-icon">
+          <button type="button mb-3"><span class="input-icon"><img  src="./assets/icons/othersize/google-favicon.png/" alt="" />&nbsp;</span>Login With Google</button>
+          <button type="button"><span class="input-icon"><img  src="./assets/icons/othersize/microsoft.png/" alt="" />&nbsp;</span>Login With Office 365</button>
+        </div>
+        <div class="seperator"><b>or</b></div>
+      <div class="form-group">
+          <input type="email" name="email" placeholder="Your Employee ID"/>
+          <span class="input-icon"><img  src="./assets/icons/24/icon-24-person-grey.png/" alt=""/></span>
+        </div>
+        <div class="form-group">
+          <input type="password" name="psw" placeholder="Your Password"/>
+          <span class="input-icon"><img src="./assets/icons/24/icon-24-password.png/" alt=""/></span>
+        </div>
+        <button class="login-btn">Login</button> 
+      </form>
+      </div>
+  
+    </div>
+    </div>
+
+    );
+}
+}
+
+export default Login;
