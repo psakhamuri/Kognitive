24e07dbecf6110c8e7078c88d0c8c200b5f1274a
psakhamuri <64536658+psakhamuri@users.noreply.github.com>
2020-05-10 12:32:34 -0400

    Updates

---
+++ precache-manifest.f05437f828e14825da7a3792ad372409.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98964225141beb391cf8aad58fbce65",
+    "url": "/kognitive/kognitive/index.html"
+  },
+  {
+    "revision": "fddeb1dbbcf095845c9b",
+    "url": "/kognitive/kognitive/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "fea847f3b84aef1ab742",
+    "url": "/kognitive/kognitive/static/css/main.171289a0.chunk.css"
+  },
+  {
+    "revision": "fddeb1dbbcf095845c9b",
+    "url": "/kognitive/kognitive/static/js/2.6ea7e5ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kognitive/kognitive/static/js/2.6ea7e5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea847f3b84aef1ab742",
+    "url": "/kognitive/kognitive/static/js/main.4a714157.chunk.js"
+  },
+  {
+    "revision": "b7632b445fea104d80fb",
+    "url": "/kognitive/kognitive/static/js/runtime-main.6ddb96db.js"
+  }
+]);
